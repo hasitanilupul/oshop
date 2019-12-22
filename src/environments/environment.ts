1a97f2d642051afa4c082b1137becf80427cf859
hasitanilupul <48640389+hasitanilupul@users.noreply.github.com>
2019-12-22 17:43:15 +0530

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyC6eUtDnBZF_EgrY4QePt4jGpCoipoboZc",
+    authDomain: "oshop-8ae13.firebaseapp.com",
+    databaseURL: "https://oshop-8ae13.firebaseio.com",
+    projectId: "oshop-8ae13",
+    storageBucket: "oshop-8ae13.appspot.com",
+    messagingSenderId: "22244845158",
+    appId: "1:22244845158:web:d9ffadd78885c5c2c9afa7"
+  }
 };
 
 /*
@@ -12,5 +21,6 @@ export const environment = {
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
+
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
