a6dcaee9c303253619dc7fdde9d48a742b7f93f0
hasitanilupul <48640389+hasitanilupul@users.noreply.github.com>
2019-12-23 14:09:35 +0530

    login

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { AuthService } from './auth.service';
+import { Router, RouterStateSnapshot } from '@angular/router';
+import 'rxjs/add/operator/map'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  path: import("@angular/router").ActivatedRouteSnapshot[];
+  route: import("@angular/router").ActivatedRouteSnapshot;
+
+  constructor(private auth: AuthService, private router:Router) { }
+
+  canActivate(route, state:RouterStateSnapshot) {
+    return this.auth.user$.map(user => {
+      if (user) return true;
+
+      this.router.navigate(['/login'], { queryParams:{ returnUr: state.url }});
+      return false;
+    });
+  }
+}
